5851f932f041ae79fbdd959a2aa11fd40d11f3a1
AndrewBauer <abauer@seekr.com>
2023-10-08 20:24:16 -0700

    Updates incl. asynchronous IIFE (Immediately Invoked Function Express.)

--- goodnewsonly/background.js
+++ goodnewsonly/background.js
@@ -3,15 +3,11 @@ console.log("Background script loaded");
 chrome.browserAction.onClicked.addListener(function(tab) {
     console.log("Insider listener: Sending message from background script");
     chrome.tabs.sendMessage(tab.id, {action: "processPage"}, function(response) {
-        console.log(response.result);  // Should log "Headlines obstructed" or "Error"
+        if(chrome.runtime.lastError){
+            console.error(chrome.runtime.lastError);
+            return;
+        }
+        console.log("Response from content script:", response.result);
     });
     console.log("Inside listener: After sending message");
 });
-
-console.log("Background script reached the end of the file");
-
-// chrome.browserAction.onClicked.addListener(function(tab) {
-//     // This block will be executed when the extension icon is clicked.
-//     // For example, you can send a message to content.js here.
-//     chrome.tabs.sendMessage(tab.id, {action: "processPage"});
-// });
--- goodnewsonly/content.js
+++ goodnewsonly/content.js
@@ -2,28 +2,32 @@ console.log("Content script loaded");
 
 // Function to obstruct specified headlines
 function obstructHeadlines(headlines) {
-    // Loop through each headline in the list
-    headlines.forEach((headlineText) => {
-        // Find the headline on the page and obstruct it.
-        // This part may vary depending on how headlines are structured in HTML.
-        const headlineElements = document.querySelectorAll('h1, h2, h3, h4, h5, h6, p, span'); // adjust selectors as needed
+    try {
+        // Loop through each headline in the list
+        headlines.forEach((headlineText) => {
+            // Find the headline on the page and obstruct it.
+            // This part may vary depending on how headlines are structured in HTML.
+            const headlineElements = document.querySelectorAll('h1, h2, h3, h4, h5, h6, p, span'); // adjust selectors as needed
 
-        headlineElements.forEach((element) => {
-            if (element.textContent.includes(headlineText)) {
-                element.style.display = 'none';  // hide the headline
-            }
+            headlineElements.forEach((element) => {
+                if (element.textContent.includes(headlineText)) {
+                    element.style.display = 'none';  // hide the headline
+                }
+            });
         });
-    });
+        console.log("Successfully obstructed headlines");
+    } catch (error) {
+        console.error("Failed to obstruct headlines:", error);
+    }
 }
 
 console.log("Received message in content script");
 
 // Listener for messages from the background script
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Idk");
-    chrome.runtime.onMessage.addListener(async function(request, sender, sendResponse) {
-        console.log("Message listener triggered, request action:", request.action);
-        if (request.action == "processPage") {
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action == "processPage") {
+        // Wrap your async code in an IIFE (Immediately Invoked Function Expression)
+        (async () => {
             try {
                 console.log("Before sending fetch request");
                 const response = await fetch('https://good-news-only-a0460683d0b8.herokuapp.com/analyze', {
@@ -34,19 +38,17 @@ document.addEventListener("DOMContentLoaded", function() {
                     body: JSON.stringify({url: window.location.href}) // or send page content
                 });
                 console.log("After sending fetch request", response);
-                if (response.ok) {
-                    const data = await response.json();
-                    console.log("Received headlines from backend:", data.headlines);
-                } else {
-                    console.log("Received an error from backend:", response.status, response.statusText);
-                }
+                const data = await response.json();
+                console.log("Received headlines from backend:", data.headlines);
                 obstructHeadlines(data.headlines);
                 sendResponse({result: "Headlines obstructed"});
             } catch (error) {
                 console.error("Error communicating with backend:", error);
                 sendResponse({result: "Error"});
             }
-        }
-        return true; // Keeps the message channel open until `sendResponse` is executed
-    });
+        })();
+
+        // Return true to indicate that the response will be sent asynchronously
+        return true;
+    }
 });
