a0b003c83fd4846344078ee8355f7f5d1666a584
abauer <abauer@seekr.com>
2023-10-09 22:46:57 -0700

    Adjust error handling in background.js

--- goodnewsonly/background.js
+++ goodnewsonly/background.js
@@ -1,18 +1,13 @@
-console.log("Background script loaded");
-
 chrome.action.onClicked.addListener(function(tab) {
     console.log("Inside listener: Sending message from background script");
     chrome.tabs.sendMessage(tab.id, {action: "processPage"}, function(response) {
-        if(chrome.runtime.lastError){
+        if (chrome.runtime.lastError) {
             console.error("Error in sendMessage:", chrome.runtime.lastError.message);
-            return;
+            return;  // Early return
         }
 
-        if (response) {
-          console.log("Response from content script:", response.result);
-        } else {
-          console.warn("No response received from content script");
-        }
+        // This code will only run if there was no error
+        console.log("Response from content script:", response ? response.result : "No response");
     });
     console.log("Inside listener: After sending message");
 });
